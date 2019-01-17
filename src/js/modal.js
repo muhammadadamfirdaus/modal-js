96de925c92d0d1681ab70f6e77e8747f07f037d5
muhammadadamfirdaus <adam@muhammadadamfirdaus>
2019-01-17 20:22:55 +0700

    solve multiple modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,24 +1,23 @@
-function modal(element){
+function modal(button, uniqueClass){
   // first, declare modal container
-  const modal = document.querySelector('.modal');
-
+  const modal = document.querySelector('.modal' + uniqueClass);
   // check if modal exists
   if(modal){
     // console.log('modal');
     // button modal on click
     window.addEventListener('click', function(e){
       // don't forget to remind user using '.button-modal'
-      if(e.target.closest('.button-modal')){
+      if(e.target.closest('.button-modal' + button)){
         // check if user click button modal
-        // console.log('inside');
+        // console.log('di dalam');
         if(modal.classList.contains('active')){
           modal.classList.remove('active');
         } else {
           modal.classList.add('active');
         }
-      } else if(e.target.closest('.modal.media')){
+      } else if(e.target.closest('.modal')){
         // and also check to closest modal media
-        // console.log('inside');
+        // console.log('di dalam');
         if(e.target.closest('.button.close')){
           // close when user hit button close modal
           modal.classList.remove('active');
@@ -28,7 +27,7 @@ function modal(element){
         }
       } else {
         // destroy everything when user out of the box...
-        // console.log('outside');
+        // console.log('di luar');
         modal.classList.remove('active');
       }
     });
